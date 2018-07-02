eb46e55288dfe1c5d3cd6ccfcf3ca47f043d9396
Annie <a10423006@yuntech.org.tw>
2018-07-03 01:08:49 +0800

    判別大小12標籤成功

--- pdfminer_part1.py
+++ pdfminer_part1.py
@@ -1,17 +1,15 @@
 from pdfminer.layout import LAParams
-from pdfminer.converter import PDFPageAggregator, HTMLConverter
+from pdfminer.converter import HTMLConverter
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
-from pdfminer.layout import LTTextBox
+import string
 from string import punctuation
-import sys
 from io import BytesIO
-from string import punctuation
-import re
 from openpyxl import load_workbook
+from bs4 import BeautifulSoup
 
 # pdf path
-document = open('/Users/anan/Desktop/pdf/Part1.pdf', 'rb')
+document = open('/Users/anan/Desktop/pdf/Part1_test.pdf', 'rb')
 # crate pdf manager
 rsrcmgr = PDFResourceManager()
 
@@ -19,28 +17,49 @@ retster = BytesIO()
 # Set parameters for analysis.
 laparams = LAParams()
 laparams.char_margin = 4  # 5 4best
-laparams.line_margin = 3  # test:3 part:5
+laparams.line_margin = 5  # test:3 part:5
 laparams.word_margin = 4
 laparams.boxes_flow = 0.5
 
-# Create a PDF page aggregator object
+# Create a PDF page TMLConverter object
 device = HTMLConverter(rsrcmgr, retster, codec='utf-8', laparams=laparams)
 # create pdf interpreter
 interpreter = PDFPageInterpreter(rsrcmgr, device)
 
-content = ""
 for page in PDFPage.get_pages(document):
     interpreter.process_page(page)
-    layout = retster.getvalue()
-    str = print(layout)
 
-    for element in layout:
-        if isinstance(element, LTTextBox):
-            obj = element.get_text()
-            content += obj
-            print(obj)
+content = retster.getvalue().decode()
 
-content.lstrip("Contact Information")  # 刪掉
+# 以 Beautiful Soup 解析 HTML 程式碼
+soup = BeautifulSoup(content, 'html.parser')
+
+# 去 br標籤
+invalid_tags = ['br']
+for tag in invalid_tags:
+    for match in soup.findAll(tag):
+        match.replaceWithChildren()
+
+f = open('/Users/anan/Desktop/pdf/test.html', 'a', encoding='utf8')
+# f.write(soup.prettify())
+
+# 找span標籤
+span_tags = soup.find_all('span')
+
+for tag in span_tags:
+    str1 = tag.get('style')
+    print(tag.string)
+    # 找出所有字型大小為12的標籤
+    # if str1.find('font-size:12px') != -1:
+    #     print()
+
+    #     # 找出所有字型大小為11的標籤
+    # if str1.find('font-size:11px') != -1:
+    #     print(tag.string)
+
+document.close()
+device.close()
+retster.close()
 
 # Excel
 # wb = load_workbook("test.xlsx")
